65abfbdee18812e0bf4d9cccc8dcd3849e1ac366
Nour Darmash <95661258+NOURDARMASH@users.noreply.github.com>
2023-12-21 18:40:50 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,18 @@
+# Project naam
+
+Trash titans platform
+
+## Trash titans
+
+Het is een platform voor jongeren voor jongeren evenals voor leraren.
+
+## Achtergrond
+
+Gebruikers dienen zich te registreren met een account, waarmee ze kunnen deelnemen aan door scholen georganiseerde wedstrijden. Hierbij kunnen punten worden verdiend door het verzamelen van plastic en recyclebare materialen.
+Daarnaast moet het platform educatieve inhoud bevatten.
+
+## Getting started
+
+Dit project bevat 9 HTML-bestanden, een JavaScript-bestand en een Sass-bestand, en binnen het sass-bestand zijn er subbestanden aan gekoppeld en daarin opgenomen. Aan het einde van het sass-bestand resulteert dit in een groot CSS-bestand met de reguliere CSS-codes.
+
+### home.html
---
+++ script.js
@@ -0,0 +1,74 @@
+//punten invoeren
+var totalPointsElement;
+var currentPoints = 0;
+
+function submitPoints() {
+  var pointsInput = document.getElementById('points-input');
+  var pointsChart = document.getElementById('points-chart');
+  var pointsValue = parseInt(pointsInput.value, 10);
+
+  if (!isNaN(pointsValue) && pointsValue > 0) {
+    var bar = document.createElement('div');
+    var barLabel = document.createElement('div');
+
+    bar.className = 'bar';
+    barLabel.className = 'bar-label';
+
+    bar.style.height = '0';
+    bar.style.transition = 'height 1s ease-out';
+
+    setTimeout(function () {
+      bar.style.height = pointsValue + 'px';
+      bar.appendChild(barLabel);
+      pointsChart.appendChild(bar);
+
+      setTimeout(function () {
+        barLabel.style.opacity = '1';
+      }, 500);
+    }, 50);
+
+    currentPoints += pointsValue;
+
+    if (!totalPointsElement) {
+      totalPointsElement = document.getElementById('total-points');
+    }
+
+    totalPointsElement.innerText = 'Totaal: ' + currentPoints;
+  }
+  document.getElementById("myForm").style.display = "none";
+}
+
+//code voor popup in de punten.html pagina
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+function closeButton() {
+  document.getElementById('myForm').style.display = "none";
+}
+
+
+///Popup wachtwoord vergeten voor de login pagina 
+
+function openPasswordPopup() {
+  document.getElementById('passwordPopup').style.display = 'block';
+}
+
+function closePasswordPopup() {
+  document.getElementById('passwordPopup').style.display = 'none';
+}
+
+function submitNewPassword() {
+  closePasswordPopup();
+}
+
+//codes voor de challanges pagina
+
+function toggleInfo(infoId) {
+  var infoElement = document.getElementById(infoId);
+  if (infoElement.style.display === 'block') {
+    infoElement.style.display = 'none';
+  } else {
+    infoElement.style.display = 'block';
+  }
+}
